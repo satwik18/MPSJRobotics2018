f5565e8569d73e56328d672af547ac6dcf3e0d65
Frank Vumbaca <fvumbaca@outlook.com>
2018-03-01 23:28:30 +0000

    baseActions.ino edited online with Bitbucket. Done during meeting.

--- Main/baseActions.ino
+++ Main/baseActions.ino
@@ -8,6 +8,7 @@
 
 // Rotates the robot by deg clockwise at a spd
 void rotate(double deg, int spd) {
+  const double threshold = 1.0;
   p.resetEncoders(); // reset encoders to clear the previous rotations
   
   // Read the current angle of the wheel (may be always 0 since line above)
@@ -25,7 +26,14 @@ void rotate(double deg, int spd) {
   leftDeg -= degreesToTurn;
 
   // Tell the robot to make the adjustments at a spd
-  p.setMotorDegrees(spd, rightDeg, spd, leftDeg);
+  //p.setMotorDegrees(spd, rightDeg, spd, leftDeg);
+  p.setMotorTargets(spd, rightDeg * 4, spd, leftDeg * 4);
+
+  // wait until robot has fully turned
+  while(abs(p.readEncoderDegrees(1) - rightDeg) > threshold && abs(p.readEncoderDegrees(2) - leftDeg) > threshold) {
+    delay(50);
+  }
+  delay(50);
 }
 
 
@@ -34,6 +42,18 @@ void forward(int mills, int spd) {
   p.setMotorSpeeds(MAX_SPEED, MAX_SPEED);
   delay(mills);
   p.setMotorSpeeds(0, 0); // turns the moters off when done
+}
+
+void forwardBy(double inches, int spd) {
+  double deg = inches / WHEEL_C * 360.0;
+  p.resetEncoders(); // reset encoders to clear the previous rotations
+  p.setMotorDegrees(spd, deg, spd, deg);
+
+  // wait until robot has fully moved
+  while(abs(p.readEncoderDegrees(1) - deg) > 1.0 && abs(p.readEncoderDegrees(2) - deg) > 1.0) {
+    delay(50);
+  }
+  delay(50);
 }
 
 
@@ -50,5 +70,30 @@ void accelerateFor(int mills, int toSpeed) {
     p.setMotorSpeeds(totalSpeed, totalSpeed); // Update the moter speed
     delay(timeStep); // Wait for the duration of this step
   }
+}
+
+// Blocks until a line is detected within threshold of accuracy
+void waitForLine(int mill, double threshold) {
+  while(p.readLineSensor(17) < threshold) {
+    delay(mill);
+  }
+}
+
+// Blocks until the distance sensor is below dist in inches
+void waitForProximityBelow(int sensorNo, double dist) {
+  const double threshold = 1.0;
+  while(abs(p.readSonicSensorIN(sensorNo) - dist) < threshold) {
+    delay(50);
+  }
+}
+
+// Set the claw to deg (note constants)
+void setClaw(int deg) {
+    p.setServoPosition(2,deg);
+}
+
+// Set the arm to deg (note constants)
+void setArm(int deg) {
+    p.setServoPosition(1,deg);
 }
 
